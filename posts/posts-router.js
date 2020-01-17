c5fc4abee81a8e57634fd59c14684c577a928a4b
Westley Feller <feller.westley@gmail.com>
2020-01-16 21:23:08 -0800

    Posts can be added to DB

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -12,4 +12,18 @@ import {
 
 const router = Router()
 
+router.post('/', ({ body, body: { title, contents } }, res) =>
+  !title || !contents
+    ? res.status(400).json({
+        errorMessage: 'Please provide title and contents for the post.',
+      })
+    : insert(body)
+        .then(post => res.status(201).json(post))
+        .catch(() =>
+          res.status(500).json({
+            error: 'There was an error while saving the post to the database',
+          })
+        )
+)
+
 export default router
