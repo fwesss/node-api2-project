0b18eb23c8fc4df4be7686752d8ab8f5a687e7ef
Westley Feller <feller.westley@gmail.com>
2020-01-16 21:44:43 -0800

    Comments can be added to posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -26,4 +26,25 @@ router.post('/', ({ body, body: { title, contents } }, res) =>
         )
 )
 
+router.post('/:id/comments', ({ body, body: { text }, params: { id } }, res) =>
+  !text
+    ? res
+        .status(400)
+        .json({ errorMessage: 'Please provide text for the comment.' })
+    : findById(id).then(posts =>
+        posts.length > 0
+          ? insertComment({ ...body, post_id: id })
+              .then(comment => res.status(201).json({ comment }))
+              .catch(() =>
+                res.status(500).json({
+                  error:
+                    'There was an error while saving the comment to the database',
+                })
+              )
+          : res.status(404).json({
+              message: 'The post with the specified ID does not exist.',
+            })
+      )
+)
+
 export default router
