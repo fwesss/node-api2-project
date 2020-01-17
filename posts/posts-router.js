6ec2e86869f561a1f5bf13c0119478f14926c4d3
Westley Feller <feller.westley@gmail.com>
2020-01-16 22:04:25 -0800

    Retrieve comments

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -13,25 +13,22 @@ import {
 const router = Router()
 
 router.post('/', ({ body, body: { title, contents } }, res) =>
-  !title || !contents
-    ? res.status(400).json({
-        errorMessage: 'Please provide title and contents for the post.',
-      })
-    : insert(body)
+  title && contents
+    ? insert(body)
         .then(post => res.status(201).json(post))
         .catch(() =>
           res.status(500).json({
             error: 'There was an error while saving the post to the database',
           })
         )
+    : res.status(400).json({
+        errorMessage: 'Please provide title and contents for the post.',
+      })
 )
 
 router.post('/:id/comments', ({ body, body: { text }, params: { id } }, res) =>
-  !text
-    ? res
-        .status(400)
-        .json({ errorMessage: 'Please provide text for the comment.' })
-    : findById(id).then(posts =>
+  text
+    ? findById(id).then(posts =>
         posts.length > 0
           ? insertComment({ ...body, post_id: id })
               .then(comment => res.status(201).json({ comment }))
@@ -45,6 +42,9 @@ router.post('/:id/comments', ({ body, body: { text }, params: { id } }, res) =>
               message: 'The post with the specified ID does not exist.',
             })
       )
+    : res
+        .status(400)
+        .json({ errorMessage: 'Please provide text for the comment.' })
 )
 
 router.get('/', (_req, res) =>
@@ -71,6 +71,22 @@ router.get('/:id', ({ params: { id } }, res) =>
         .status(500)
         .json({ error: 'The post information could not be retrieved.' })
     )
+)
+
+router.get('/:id/comments', ({ params: { id } }, res) =>
+  findById(id).then(posts =>
+    posts.length > 0
+      ? findPostComments(id)
+          .then(comments => res.status(200).json({ comments }))
+          .catch(() =>
+            res.status(500).json({
+              error: 'The comments information could not be retrieved.',
+            })
+          )
+      : res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' })
+  )
 )
 
 export default router
